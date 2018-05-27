4da828b919158a721d5f6848267c2c4f85507035
M.Nabil <monagomaa@smartera3s.com>
2018-05-27 21:39:44 +0200

    updating pixel_a and pixel_b

--- src/main/java/app/services/Constants.java
+++ src/main/java/app/services/Constants.java
@@ -6,7 +6,9 @@ package app.services;
 public class Constants {
   public static final  String DEFAULT_SEQUANCE[] = {"a", "a", "b", "a", "b", "a", "b", "a", "b", "a"};
   public static final  int DEFAULT_CURRENT = 1;
-  public static final String PIXEL_A = "<p>Hi A</p>";
-  public static final String PIXEL_B = "<p>Hi B</p>";
+  public static final String PIXEL_A = "<script>"
+ + "$(document).ready(function(){var px='<iframe src=\"https://yeembe.go2cloud.org/aff_l?offer_id=' + store.get('Offer_ID') + '\" scrolling=\"no\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>';$('#log').html(px);})"
+ +"</script>";
+  public static final String PIXEL_B = "<p></p>";
   public static final int NUMBER_OF_SEQ = 10;
 }
