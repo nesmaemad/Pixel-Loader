53bfbc9594c8178e2ff34e33c66c841700a1ec65
nesmaemad <nesma.emad45@gmail.com>
2018-07-13 21:33:36 +0200

    adding scripts tag to the constant

--- src/main/java/app/services/Constants.java
+++ src/main/java/app/services/Constants.java
@@ -6,7 +6,7 @@ package app.services;
 public class Constants {
   public static final  String DEFAULT_SEQUANCE[] = {"a", "a", "b", "a", "b", "a", "b", "a", "b", "a"};
   public static final  int DEFAULT_CURRENT = 1;
-  public static final String PIXEL_A =  "$(document).ready(function(){var px='<iframe src=\"https://yeembe.go2cloud.org/aff_l?offer_id=' + store.get('Offer_ID') + '\" scrolling=\"no\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>';$('#log').html(px);})";
+  public static final String PIXEL_A =  "<script type='text/javascript'>$(document).ready(function(){var px='<iframe src=\"https://yeembe.go2cloud.org/aff_l?offer_id=' + store.get('Offer_ID') + '\" scrolling=\"no\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>';$('#log').html(px);})</script>";
   public static final String PIXEL_B = "<html><body><p></p></body></html>";
   public static final int NUMBER_OF_SEQ = 10;
 }
