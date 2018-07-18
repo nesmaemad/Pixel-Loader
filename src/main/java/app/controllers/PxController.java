0b3faea3efea337eb2f7aef69bd6ba518b0ca58a
nesmaemad <nesma.emad45@gmail.com>
2018-07-18 20:51:39 +0200

    adding validation on the id

--- src/main/java/app/controllers/PxController.java
+++ src/main/java/app/controllers/PxController.java
@@ -11,32 +11,39 @@ public class PxController extends AppController {
 
     public void index(){
         String pid = param("pid");
-        PixelLoader pxModel = new PixelLoader();
+        if(pid != null) {
+            PixelLoader pxModel = new PixelLoader();
 
-        PixelLoader getInstance = PixelLoader.findFirst("pid = ?", pid);
+            PixelLoader getInstance = PixelLoader.findFirst("pid = ?", pid);
 
-        if(getInstance == null){
-            getInstance = pxModel.create(Integer.parseInt(pid),
-                                        Constants.DEFAULT_SEQUANCE,
-                                        Constants.PIXEL_A,
-                                        Constants.PIXEL_B,
-                                        Constants.DEFAULT_CURRENT);
+            if (getInstance == null) {
+                getInstance = pxModel.create(Integer.parseInt(pid),
+                        Constants.DEFAULT_SEQUANCE,
+                        Constants.PIXEL_A,
+                        Constants.PIXEL_B,
+                        Constants.DEFAULT_CURRENT);
+            }
+
+            int getCurrent = getInstance.getInteger("current");
+            String currentSeqNumber = "seq" + getCurrent;
+            String currentSeq = getInstance.getString(currentSeqNumber);
+            String currentPixel = getInstance.getString(("pixel_" + currentSeq));
+            // number of sequences base 1
+            getCurrent = (getCurrent + 1) % (Constants.NUMBER_OF_SEQ + 1);
+            if (getCurrent == 0)
+                getCurrent++;
+            getInstance.set("current", getCurrent);
+            getInstance.saveIt();
+            respond(currentPixel)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .contentType("text/javascript")
+                    .status(200);
+        }else{
+            respond(Constants.PID_NULL)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .contentType("text/javascript")
+                    .status(200);
         }
-
-        int getCurrent = getInstance.getInteger("current") ;
-        String currentSeqNumber = "seq" + getCurrent;
-        String currentSeq = getInstance.getString(currentSeqNumber);
-        String currentPixel = getInstance.getString(("pixel_" + currentSeq));
-        // number of sequences base 1
-        getCurrent = (getCurrent + 1 ) % (Constants.NUMBER_OF_SEQ + 1);
-        if(getCurrent == 0)
-            getCurrent++;
-        getInstance.set("current", getCurrent);
-        getInstance.saveIt();
-        respond(currentPixel)
-                .header("Access-Control-Allow-Origin", "*" )
-                .contentType("text/javascript")
-                .status(200);
 
     }
 }
--- src/main/java/app/services/Constants.java
+++ src/main/java/app/services/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
   public static final  int DEFAULT_CURRENT = 1;
   public static final String PIXEL_A =  "$(document).ready(function(){var px='<iframe src=\"https://yeembe.go2cloud.org/aff_l?offer_id=' + store.get('Offer_ID') + '\" scrolling=\"no\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>';$('#log').html(px);})";
   public static final String PIXEL_B = "<html><body><p></p></body></html>";
+  public static final String PID_NULL = "<html><body></body></html>";
   public static final int NUMBER_OF_SEQ = 10;
 }
