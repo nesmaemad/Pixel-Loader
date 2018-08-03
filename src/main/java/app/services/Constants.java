471a7cfe76fc47dbaa34caf1858a70171f3bae8c
nesmaemad <nesma.emad45@gmail.com>
2018-08-03 17:31:56 +0200

    editing PIXEL_B

--- src/main/java/app/services/Constants.java
+++ src/main/java/app/services/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
   public static final  String DEFAULT_SEQUANCE[] = {"a", "a", "b", "a", "b", "a", "b", "a", "b", "a"};
   public static final  int DEFAULT_CURRENT = 1;
   public static final String PIXEL_A =  "$(document).ready(function(){var px='<iframe src=\"https://yeembe.go2cloud.org/aff_l?offer_id=' + store.get('Offer_ID') + '\" scrolling=\"no\" frameborder=\"0\" width=\"1\" height=\"1\"></iframe>';$('#log').html(px);})";
-  public static final String PIXEL_B = "<html><body><p></p></body></html>";
+  public static final String PIXEL_B = "";
   public static final String PID_NULL = "";
   public static final int NUMBER_OF_SEQ = 10;
 }
